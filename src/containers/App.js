b94451e1906540efb574b70f5420156122b5a419
Ragavendran Bala <balasubramaniamragavendran78@gmail.com>
2023-11-08 11:20:34 +0530

    updated to the app logic to use Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,73 @@
-import React, {Component} from "react";
+// import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "./ErrorBoundry";
 import "./App.css"
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchFieldValue: ''
-        }
-    }
+// class App extends Component {
+function App() {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchFieldValue: ''
+    //     }
+    // }
 
-    componentDidMount() {
+    const [robots, setRobots] = useState([]);
+    const [searchFieldValue, setSearchFieldValue] = useState('');
+
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response => response.json())
+    //         .then(users => {
+    //             this.setState({robots: users})
+    //         });
+    // }
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => {
-                this.setState({robots: users})
+                setRobots(users)
             });
+    }, []);
+
+
+    // OnValueChanged = (event) => {
+    //     this.setState({searchFieldValue: event.target.value});
+    // }
+    const OnValueChanged = (event) => {
+        setSearchFieldValue(event.target.value);
     }
 
-    OnValueChanged = (event) => {
-        this.setState({searchFieldValue: event.target.value});
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchFieldValue.toLowerCase());
+    });
+
+    if (robots.length === 0) {
+        return (
+            <div className='tc'>
+                <h1>Loading Robots</h1>
+            </div>
+        )
+    } else {
+        return (
+            <div className='tc'>
+                <h1 className='f1'>Robo Friends</h1>
+                <SearchBox OnValueChangedCallback={OnValueChanged}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robotsList={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+
+            </div>
+        );
     }
 
-    render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchFieldValue.toLowerCase());
-        });
-
-        if (this.state.robots.length === 0) {
-            return (
-                <div className='tc'>
-                    <h1>Loading Robots</h1>
-                </div>
-            )
-        } else {
-            return (
-                <div className='tc'>
-                    <h1 className='f1'>Robo Friends</h1>
-                    <SearchBox OnValueChangedCallback={this.OnValueChanged}/>
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robotsList={filteredRobots}/>
-                        </ErrorBoundry>
-                    </Scroll>
-
-                </div>
-            );
-        }
-    }
 }
 
 export default App;
